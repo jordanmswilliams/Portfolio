7b515b4e24880341fe0abfbc9b0e16e9613f2413
Robin <rocwilliams@icloud.com>
2019-04-28 10:11:23 -0500

    Added DS_Store to .gitignore

--- scripts/apps.js
+++ scripts/apps.js
@@ -15,3 +15,17 @@ function myFunction() {
     navbar.classList.remove("sticky");
   }
 }
+
+M.AutoInit();
+
+var instance = M.Carousel.init({
+  fullWidth: true,
+  indicators: true
+});
+
+// Or with jQuery
+
+$('.carousel.carousel-slider').carousel({
+  fullWidth: true,
+  indicators: true
+});
